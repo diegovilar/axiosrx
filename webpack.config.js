a2fa95a3dfe4cdcff5df68e7f4f9741b8d010bc2
Diego Vilar <diegovilar@gmail.com>
2018-02-18 14:25:04 -0300

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const clone = require('clone');
 const path = require('path');
 const webpack = require("webpack");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+
+function cloneConfig(source) {
+    const result = {};
+
+    for (var name in source) {
+        result[name] = (name == "plugins") ? source[name].slice(0) : clone(source[name]);
+    }
+
+    return result;
+}
 
 const sharedConfig = {
-    entry: './build/esm/index.js',
+    entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'build/bundles'),
         library: "axiosrx",
@@ -13,58 +26,59 @@ const sharedConfig = {
     module: {
         rules: [
             {
+                test: /\.ts$/,
+                use: [{
+                    loader: "awesome-typescript-loader",
+                    options: {
+                        configFileName: "./src/tsconfig.bundles.json",
+                        useCache: true
+                    }
+                }]
+            }, {
                 test: /\.js$/,
                 use: ["source-map-loader"],
                 enforce: "pre"
             }
         ]
     },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
     externals: {
         rxjs: "Rx"
-    }
+    },
+    plugins: [
+        new CheckerPlugin(),
+        new HardSourceWebpackPlugin()
+    ]
 };
 
 // Lib
-const libConfig = Object.assign({}, sharedConfig, {
-    output: Object.assign({}, sharedConfig.output, {
-        filename: 'axiosrx.js',
-    }),
-    externals: Object.assign({}, sharedConfig.externals, {
-        axios: "axios"
-    }),
-    plugins: [
-    ]
-});
+const libConfig = cloneConfig(sharedConfig);
+libConfig.output.filename = 'axiosrx2.js';
+libConfig.externals.axios = "axios";
 
-const libMinConfig = Object.assign({}, libConfig, {
-    output: Object.assign({}, libConfig.output, {
-        filename: 'axiosrx.min.js',
-    }),
-    plugins: libConfig.plugins.concat([
-        new UglifyJsPlugin({
-            sourceMap: true
-        })
-    ])
-});
+// Lib min
+const libMinConfig = cloneConfig(libConfig);
+libMinConfig.output.filename = 'axiosrx2.min.js';
+libMinConfig.plugins.push(
+    new UglifyJsPlugin({
+        sourceMap: true
+    })
+);
 
-const bundleConfig = Object.assign({}, sharedConfig, {
-    output: Object.assign({}, sharedConfig.output, {
-        filename: 'axiosrx.bundle.js',
-    }),
-    plugins: [
-    ]
-});
+// Bundle
+const bundleConfig = cloneConfig(sharedConfig);
+bundleConfig.output.filename = 'axiosrx2.bundle.js';
 
-const bundleMinConfig = Object.assign({}, bundleConfig, {
-    output: Object.assign({}, bundleConfig.output, {
-        filename: 'axiosrx.bundle.min.js',
-    }),
-    plugins: bundleConfig.plugins.concat([
-        new UglifyJsPlugin({
-            sourceMap: true
-        })
-    ])
-})
+// Bundle min
+const bundleMinConfig = cloneConfig(bundleConfig);
+bundleMinConfig.output.filename = 'axiosrx2.bundle.min.js';
+bundleMinConfig.plugins.push(
+    new UglifyJsPlugin({
+        sourceMap: true
+    })
+);
 
 module.exports = [
     libConfig,
