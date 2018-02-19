9cb798ddc1c4beee047a1b07880ea5284ba67f48
Diego Vilar <diegovilar@gmail.com>
2018-02-19 00:21:37 -0300

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 const webpack = require("webpack");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { CheckerPlugin } = require('awesome-typescript-loader');
+
+const TS_CONFIG_FILE = "./src/tsconfig.bundles.json";
 
 function cloneConfig(source) {
     const result = {};
@@ -18,7 +21,7 @@ function cloneConfig(source) {
 const sharedConfig = {
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'build/bundles'),
+        path: path.resolve(__dirname, 'build/browser'),
         library: "axiosrx",
         libraryTarget: "umd"
     },
@@ -30,7 +33,7 @@ const sharedConfig = {
                 use: [{
                     loader: "awesome-typescript-loader",
                     options: {
-                        configFileName: "./src/tsconfig.bundles.json",
+                        configFileName: TS_CONFIG_FILE,
                         useCache: true
                     }
                 }]
@@ -49,18 +52,21 @@ const sharedConfig = {
     },
     plugins: [
         new CheckerPlugin(),
-        new HardSourceWebpackPlugin()
+        new HardSourceWebpackPlugin(),
+
+        // Caso façamos uso de paths nas configuracoes do compilador do typescript
+        // new TsconfigPathsPlugin({ configFile: TS_CONFIG_FILE })
     ]
 };
 
 // Lib
 const libConfig = cloneConfig(sharedConfig);
-libConfig.output.filename = 'axiosrx2.js';
+libConfig.output.filename = 'axiosrx.js';
 libConfig.externals.axios = "axios";
 
 // Lib min
 const libMinConfig = cloneConfig(libConfig);
-libMinConfig.output.filename = 'axiosrx2.min.js';
+libMinConfig.output.filename = 'axiosrx.min.js';
 libMinConfig.plugins.push(
     new UglifyJsPlugin({
         sourceMap: true
@@ -69,11 +75,11 @@ libMinConfig.plugins.push(
 
 // Bundle
 const bundleConfig = cloneConfig(sharedConfig);
-bundleConfig.output.filename = 'axiosrx2.bundle.js';
+bundleConfig.output.filename = 'axiosrx.bundle.js';
 
 // Bundle min
 const bundleMinConfig = cloneConfig(bundleConfig);
-bundleMinConfig.output.filename = 'axiosrx2.bundle.min.js';
+bundleMinConfig.output.filename = 'axiosrx.bundle.min.js';
 bundleMinConfig.plugins.push(
     new UglifyJsPlugin({
         sourceMap: true
